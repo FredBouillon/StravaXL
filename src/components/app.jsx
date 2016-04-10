dc7980e6a1bc76cef68cc139ffcd82a38fe03240
Frederik Bouillon <frederikbouillon@gmail.com>
2016-04-10 21:17:50 +0200

    API request test

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class App extends React.Component {
   constructor() {
@@ -11,5 +12,16 @@ export default class App extends React.Component {
         <h1>StravaXL !</h1>
       </div>
     )
+  }
+
+  componentDidMount() {
+     
+    axios.get('http://localhost:8080/api/persons')
+      .then(function (response) {
+        console.log('axios result get persons', response);
+      })
+      .catch(function (response) {
+        console.log(response);
+      });
   }
 }
