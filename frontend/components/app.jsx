8e4bc807cda8d061b2b60eb58a17e0c27c6cb547
Frederik Bouillon <frederikbouillon@gmail.com>
2016-04-13 20:54:37 +0200

    Test first api call

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,22 +4,29 @@ import axios from 'axios';
 export default class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      athlete: ''
+    };
   }
 
   render() {
     return(
       <div>
         <h1>StravaXL !</h1>
+        <p>{this.state.athlete.firstname}</p>
       </div>
     )
   }
 
   componentDidMount() {
     console.log('NODE_ENV', process.env.NODE_ENV);
-
-    axios.get('http://localhost:8080/api/athlete')
+    var self = this;
+    axios.get(window.location.origin + '/api/athlete')
       .then(function (response) {
-        console.log('axios result get persons', response);
+        console.log('axios result get athlete', response);
+        self.setState({
+          athlete: response.data
+        })
       })
       .catch(function (response) {
         console.log(response);
