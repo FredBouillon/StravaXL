85d0f46c73bdaf2440b800989788f5fb5aed939c
Frederik Bouillon <frederikbouillon@gmail.com>
2016-04-28 23:09:27 +0200

    New components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import AppBar from 'material-ui/appBar';
+// import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/MenuItem';
+import {red900} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: red900,
+  },
+});
 
 export default class App extends React.Component {
   constructor() {
@@ -25,10 +37,18 @@ export default class App extends React.Component {
 
   render() {
     return(
-      <div>
-        <h1>{'StravaXL !'}</h1>
-        <p>{this.state.athlete.firstname}</p>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <AppBar
+            title="StravaXL"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          >
+            <MenuItem style={{color: 'white'}} primaryText="Summary" />
+            <MenuItem style={{color: 'white'}} primaryText="Activities" />
+          </AppBar>
+          <p>{this.state.athlete.firstname}</p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
