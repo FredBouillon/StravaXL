fbb45cd33c59d2954829e70a551d825a9acb25dc
Frederik Bouillon <frederikbouillon@gmail.com>
2016-04-29 19:57:14 +0200

    Added api call

--- frontend/components/summary.jsx
+++ frontend/components/summary.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Summary extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      athlete: ''
+    };
+  }
+  componentDidMount() {
+    var self = this;
+    axios.get(window.location.origin + '/api/athlete')
+      .then(function (response) {
+        console.log('axios result get athlete', response);
+        self.setState({
+          athlete: response.data
+        });
+      })
+      .catch(function (response) {
+        console.log(response);
+      });
+  }
   render() {
     return (
       <div>
         <div>{'Summary'}</div>
+        <p>{this.state.athlete.firstname}</p>
       </div>
     );
   }
