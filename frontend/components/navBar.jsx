f6df080bce39ceebe83284d28bea94783cbd7b06
Frederik Bouillon <frederikbouillon@gmail.com>
2016-04-29 18:41:38 +0200

    Fixed typo

--- frontend/components/navBar.jsx
+++ frontend/components/navBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import AppBar from 'material-ui/appBar';
+import AppBar from 'material-ui/AppBar';
 import MenuItem from 'material-ui/MenuItem';
-import { ReactRouter, Link } from 'react-router';
+import { Link } from 'react-router';
+
 
 export default class Activities extends React.Component {
   render() {
     return (
       <AppBar title="StravaXL" iconClassNameRight="muidocs-icon-navigation-expand-more">
-        <Link to="/summary" style={{'textDecoration': 'initial'}}><MenuItem style={{color: 'white'}} primaryText="Summary" /></Link>
-        <Link to="/activities" style={{'textDecoration': 'initial'}}><MenuItem style={{color: 'white'}} primaryText="Activities" /></Link>
+        <Link to="/summary" style={{ 'textDecoration': 'initial' }}><MenuItem style={{ color: 'white' }} primaryText="Summary" /></Link>
+        <Link to="/activities" style={{ 'textDecoration': 'initial' }}><MenuItem style={{ color: 'white' }} primaryText="Activities" /></Link>
       </AppBar>
     );
   }
