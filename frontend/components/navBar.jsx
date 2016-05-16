b9d14bebbcbb98b06e2eb2f384ad6ded14fd2bc9
Frederik Bouillon <frederikbouillon@gmail.com>
2016-05-16 23:09:48 +0200

    Restyled navbar with materialize-css

--- frontend/components/navBar.jsx
+++ frontend/components/navBar.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import MenuItem from 'material-ui/MenuItem';
 import { Link } from 'react-router';
 
 
-export default class Activities extends React.Component {
+export default class NavBar extends React.Component {
   render() {
     return (
-      <AppBar title="StravaXL" iconClassNameRight="muidocs-icon-navigation-expand-more">
-        <Link to="/summary" style={{ 'textDecoration': 'initial' }}><MenuItem style={{ color: 'white' }} primaryText="Summary" /></Link>
-        <Link to="/activities" style={{ 'textDecoration': 'initial' }}><MenuItem style={{ color: 'white' }} primaryText="Activities" /></Link>
-      </AppBar>
+      <div>
+        <ul id="dropdown1" className="dropdown-content">
+          {/*<li><a href="#!">one</a></li>
+          <li><a href="#!">two</a></li>
+          <li className="divider"></li>*/}
+          <li><a onClick={this.props.onLogout}>Logout</a></li>
+        </ul>
+        <nav>
+          <div className="nav-wrapper">
+            <Link to="/"><a className="brand-logo">StravaXL</a></Link>
+            <a href="" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><Link to="/summary">Summary</Link></li>
+              <li><Link to="/activities">Activities</Link></li>
+              <li>
+                <a className="dropdown-button" data-activates="dropdown1">
+                  <img src={this.props.athlete.profile_medium} className="circle"/>
+                  <i className="material-icons right">arrow_drop_down</i>
+                </a>
+              </li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">Javascript</a></li>
+              <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
